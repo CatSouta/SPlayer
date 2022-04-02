6d5ee83d527524f9901d74b0cf528005660a62eb
F_picacho <2314008827@qq.com>
2022-04-03 06:16:47 +0800

    [add modules] development dependencies autoprefixer css-loader postcss postcss-loader [modify] package.json 使css处理模块兼容市面上大部分厂商浏览器，且最低支持最新版本的前两个版本 [modify] webpack.config.js 完善生产环境与测试环境配置 [add config] postcss.config.js 编辑了模块基础引用

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("hello world")
+import "./style/main.scss";
+
+console.log("hello world");
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+    ],
+  },
+  mode: "production",
+};
